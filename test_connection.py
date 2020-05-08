53b19458083faee6e07b8aa7210fcccb25b6fa8b
Siwanont Sittinam <siwanont1@gmail.com>
2020-05-08 11:35:35 +0700

    Add Test_connection

---
+++ test_connection.py
@@ -0,0 +1,67 @@
+def inputdata():
+    prefixs = "192.168.1"
+    suffixs = "10-20, 30, 37-99, 45, 252"
+    ips = generateIP(prefixs, suffixs)
+    vlan = "99"
+    vrf = "default"
+    port_s = "ICMP, 3725"
+    ports = generatePort(port_s)
+    return ips, vlan, vrf, ports
+
+def generateIP(prefixs, suffixs):
+    ips_l = []
+    for suffix in suffixs.split(', '):
+        if suffix == "":
+            continue
+        if "-" in suffix:
+            a, b = suffix.split('-')
+            for j in range(int(a), int(b) + 1, 1):
+                ips_l.append(prefixs + "." + str(j))
+        else:
+            ips_l.append(prefixs + "." + suffix)
+    return ips_l
+
+def generatePort(ports):
+    ports_l = []
+    for port in ports.split(', '):
+        if port == "":
+            continue
+        if "-" in port:
+            a, b = port.split('-')
+            for j in range(int(a), int(b) + 1, 1):
+                ports_l.append(str(j))
+        else:
+            ports_l.append(port)
+    return ports_l
+
+def printInfo(ips, vlan, vrf, ports):
+    print(">>>" + "="*50 +">>>")
+    print(">>> Generate Connection Testing Command >>>")
+    print(">>>" + "="*50 +">>>")
+    print("IPs : " + ", ".join(sorted(ips)))
+    print("VLAN : " + vlan)
+    print("VRF : " + vrf)
+    print("PORTs : " + ", ".join(sorted(ports)))
+    print(">>>" + "="*50 +">>>")
+
+def generateTraceroute(ip, vlan, vrf):
+    print("traceroute " + ip + " source v " + vlan + " v " + vrf)
+
+def generateTelnet(ip, port, vlan, vrf):
+    print("telnet " + ip + " " + port + " s v " + vlan + " v " + vrf)
+
+def generateCommand(ips, vlan, vrf, ports):
+    print(">>> Generate Command >>>")
+    print(">>>" + "="*40 +">>>")
+    for ip in ips:
+        for port in ports:
+            if port == "ICMP":
+                generateTraceroute(ip, vlan, vrf)
+            else:
+                generateTelnet(ip, port, vlan, vrf)
+        print(">>>" + "="*40 +">>>")
+
+if __name__ == "__main__":
+    ips, vlan, vrf, ports = inputdata()
+    printInfo(ips, vlan, vrf, ports)
+    generateCommand(ips, vlan, vrf, ports)
